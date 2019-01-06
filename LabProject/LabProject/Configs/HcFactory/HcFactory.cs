c954c687b9970c1ae20858355dd7654b24d2c271
Denis TDR <denis.tdr@gmail.com>
2019-01-07 01:57:13 +0200

    renamed input/output files, relativized some paths

--- LabProject/LabProject/Configs/HcFactory/HcFactory.cs
+++ LabProject/LabProject/Configs/HcFactory/HcFactory.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using LabProject.Hashing;
 
 namespace LabProject.Configs.HcFactory
@@ -8,36 +9,34 @@ namespace LabProject.Configs.HcFactory
     {
         public static ConfigModel BuildHardcodedModel()
         {
+            const string testsDirectory = @"../../TestApps/tests";
             var cm = new ConfigModel {Programs = new List<ProgramModel>()};
             var pm1 = new ProgramModel
             {
                 ExecutablePath = @"C:\Program Files\dotnet\dotnet.exe",
-                Arguments = @"D:\Projects\UPT\master\tcs\lab\TestApps\build\SumApp.dll",
-                WorkingDirectory = @"D:\Projects\UPT\master\tcs\lab\TestApps\tests",
+                Arguments = @"../../TestApps/build/SumApp.dll",
+                WorkingDirectory = testsDirectory,
                 Tests = new List<TestModel>()
             };
-            for (var i = 1; i <= 25; i++)
-            {
-                var numberStr = string.Format(i.ToString("D3"), i);
-                pm1.Tests.Add(new TestModel("input" + numberStr + ".in", "sum" + numberStr + ".out"));
-            }
+
 
             cm.Programs.Add(pm1);
 
             var pm2 = new ProgramModel
             {
                 ExecutablePath = @"C:\Program Files\dotnet\dotnet.exe",
-                Arguments = @"D:\Projects\UPT\master\tcs\lab\TestApps\build\SortApp.dll",
-                WorkingDirectory = @"D:\Projects\UPT\master\tcs\lab\TestApps\tests",
+                Arguments = @"../../TestApps/build/SortApp.dll",
+                WorkingDirectory = testsDirectory,
                 Tests = new List<TestModel>()
             };
-            for (var i = 1; i <= 25; i++)
-            {
-                var numberStr = string.Format(i.ToString("D3"), i);
-                pm2.Tests.Add(new TestModel("input" + numberStr + ".in", "sort" + numberStr + ".out"));
-            }
 
             cm.Programs.Add(pm2);
+
+            foreach (var file in Directory.GetFiles(testsDirectory).Where(file => file.Contains("test-")))
+            {
+                pm1.Tests.Add(new TestModel(file, file.Replace("test-", "sum-").Replace(".in", ".out")));
+                pm2.Tests.Add(new TestModel(file, file.Replace("test-", "sort-").Replace(".in", ".out")));
+            }
 
             return cm;
         }
